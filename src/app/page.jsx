71c34cd2121cd7dfd6698e01a13fde01728c2252
TiagoGrebogi <tiagong2005@gmail.com>
2024-06-24 13:04:41 -0300

    feat: first commit, adding the header

---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <html className="bg-slate-950">
+
+
+      <main className="">
+        <header className="">
+          <h1 className="text-white font-Rubik font-bold text-3xl text-center py-3 tracking-wide">
+            Portfólio
+          </h1>
+          <div className="h-0.5 bg-gradient-to-r from-violet-950 to-purple-950"></div>
+        </header>
+
+      </main>
+    </html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { Fira_Code, Rubik } = require('next/font/google');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,6 +13,10 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        firaCode: ['Fira Code', 'sans-serif'],
+        Rubik: ['Rubik', 'sans-serif'],
       },
     },
   },
